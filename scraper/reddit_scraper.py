793bc5ab0bcf9f93c2d3acf5adeb56074a18712b
Max Margenot <max.margenot@gmail.com>
2017-09-30 04:07:43 -0400

    Wrote top submission title scraper

---
+++ scraper/reddit_scraper.py
@@ -0,0 +1,31 @@
+import praw
+import numpy as np
+
+
+# Define the subreddits we want to look at
+data_dir = '../data/'
+data_file = 'subreddits.txt'
+
+# Access reddit with praw
+reddit = praw.Reddit('ConspiracyCorpusScraper')
+
+
+# Iterate over subreddits
+
+with open(data_dir+data_file) as f:
+    for subreddit_name in f:
+        subreddit = reddit.subreddit(subreddit_name.strip()) 
+        try:
+            top_submissions = subreddit.top(limit=30)
+        except:
+            continue
+
+        post_text = []
+        for post in top_submissions:
+            try:
+               post_text.append(post.selftext) 
+            except:
+                continue
+        print(post_text)
+
+
